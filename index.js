3c643d6d03a0966ae621736739cf7aac55c60928
Caroline <contatocarolinefeijo@gmail.com>
2022-10-03 14:17:51 -0300

    crud_api

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8000
+var cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/assets', express.static(__dirname + '/assets'))
+
+require('./src/controllers/RecipeController')(app);
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando no endereço http://localhost:${port}`)
+})
---
+++ src/services/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+let crud = "crud";
+
+mongoose.connect(`mongodb+srv://saberdigital:saberdigital@saber-digital-db.bbpgwpb.mongodb.net/${crud}`, { useNewUrlParser: true, useUnifiedTopology: true },
+
+    function(err) {
+        if (err) throw err;
+        else {
+            console.log('sucess connection with db');
+        }
+    });
+
+module.exports = mongoose;
